dea9e08d9ca6d3e1e8312efc528398121d427f63
pedro-drosa <pedro_drosa@hotmail.com>
2022-10-13 11:00:15 -0300

    feat: add font size token

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      "2xl": 32,
+    },
     colors: {
       "black-900": "#000000",
       "gray-900": "#121214",
