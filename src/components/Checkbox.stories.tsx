cf2a45ecbfa6174c68bc952ecea37a4f790eb7e7
pedro-drosa <pedro_drosa@hotmail.com>
2022-10-13 17:46:11 -0300

    feat(Checkbox.tsx): create component

---
+++ src/components/Checkbox.stories.tsx
@@ -0,0 +1,11 @@
+import { Checkbox, CheckboxProps } from "./Checkbox";
+import { StoryObj, Meta } from "@storybook/react";
+
+export default {
+  title: "Components/Checkbox",
+  component: Checkbox,
+  args: {},
+  argTypes: {},
+} as Meta<CheckboxProps>;
+
+export const Default: StoryObj<CheckboxProps> = {};
