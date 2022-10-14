50e98bcab031b99017a93268a1b0011c091f195f
pedro-drosa <pedro_drosa@hotmail.com>
2022-10-14 14:39:48 -0300

    refactor(SignIn.tsx): create page

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,11 @@
+import { SignIn } from "./SignIn";
+import { StoryObj, Meta } from "@storybook/react";
+
+export default {
+  title: "Pages/SignIn",
+  component: SignIn,
+  args: {},
+  argTypes: {},
+} as Meta;
+
+export const Default: StoryObj = {};
