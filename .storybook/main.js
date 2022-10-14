de7aea98acdea69a0f4953acb17dfa87b13b811c
pedro-drosa <pedro_drosa@hotmail.com>
2022-10-14 14:50:22 -0300

    chore: add @storybook/addon-interactions
    - @storybook/jest
    
    - @storybook/testing-library
    
    - @storybook/test-runner

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
+    "@storybook/addon-interactions",
   ],
   framework: "@storybook/react",
   core: {
@@ -12,6 +13,7 @@ module.exports = {
   },
   features: {
     storyStoreV7: true,
+    interactionsDebugger: true,
   },
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
